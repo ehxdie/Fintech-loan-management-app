c97a67759a5a897f47d9a810912d3a71feabd151
Edidiong <ephraimedy@gmail.com>
2025-01-03 14:06:25 +0100

    Add initial project structure with configuration files, application properties, and basic components

---
+++ backend/fintech_loan_app/src/main/java/dev_eddy/fintech_loan_app/FintechLoanAppApplication.java
@@ -0,0 +1,13 @@
+package dev_eddy.fintech_loan_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechLoanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechLoanAppApplication.class, args);
+	}
+
+}
---
+++ backend/fintech_loan_app/src/test/java/dev_eddy/fintech_loan_app/FintechLoanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev_eddy.fintech_loan_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechLoanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
