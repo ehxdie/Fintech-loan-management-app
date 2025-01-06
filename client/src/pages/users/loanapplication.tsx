adc940998a8aa144c8f2a4c3dc95de6126658f6b
Edidiong <ephraimedy@gmail.com>
2025-01-06 12:53:56 +0100

    Fix import path for loanAPI and add Vercel configuration file

--- client/src/pages/users/loanapplication.tsx
+++ client/src/pages/users/loanapplication.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { loanAPI } from '../../services/api';
+import { loanAPI } from '../../services/api.ts';
 
 interface Loan {
   id: number;
